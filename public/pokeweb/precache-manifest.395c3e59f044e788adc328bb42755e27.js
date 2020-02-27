45accbdb01e8231be4f199f48064163fea25d6b4
Lauren Barry <laurenbarry00@gmail.com>
2020-02-27 12:07:03 -0800

    Added Pokeweb live site demo

---
+++ public/pokeweb/precache-manifest.395c3e59f044e788adc328bb42755e27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c04e40fe3ce4891121aad3b93dcb20",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d092bc240065d5e6e295",
+    "url": "./static/css/main.db2f9b88.chunk.css"
+  },
+  {
+    "revision": "55c4eade8a8d66c8810c",
+    "url": "./static/js/2.64e4b734.chunk.js"
+  },
+  {
+    "revision": "7ece30537ae0b88e81b0b3d9747b5d58",
+    "url": "./static/js/2.64e4b734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d092bc240065d5e6e295",
+    "url": "./static/js/main.4441db07.chunk.js"
+  },
+  {
+    "revision": "ad7aeedbda600c93a55d",
+    "url": "./static/js/runtime-main.fbfd147f.js"
+  }
+]);

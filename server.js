02a32d6b69cc6d9adc668d725613e11535d88b19
Lauren Barry <laurenbarry00@gmail.com>
2020-02-27 12:07:34 -0800

    Created express server with endpoints for live demos

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = 5000;
+
+console.log("Starting up server...")
+
+// setup static paths
+app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "build", "starbird"), {
+    extensions: ['html', 'htm']
+}));
+app.use(express.static(path.join(__dirname, "build", "pokeweb"), {
+    extensions: ['html', 'htm']
+}));
+app.use(express.static(path.join(__dirname, "build", "bombflip"), {
+    extensions: ['html', 'htm']
+}));
+
+// ----- Portfolio homepage
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// ----- BombFlip game demo endpoints
+app.get("/bombflip", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "bombflip", "bombflip.html"));
+});
+app.get("/bombflip/play", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "bombflip", "game.html"));
+});
+app.get("/bombflip/tutorial", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "bombflip", "tutorial.html"));
+});
+
+// ----- Pokeweb site demo endpoint (only need the one since it's a single-page react app)
+app.get("/pokeweb", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "pokeweb", "pokeweb.html"));
+});
+
+// ----- Starbird game demo endpoints
+app.get("/starbird/about", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "starbird", "design.html"));
+});
+app.get("/starbird/resources", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "starbird", "resources.html"));
+});
+app.get("/starbird", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "starbird", "starbird.html"));
+});
+
+// start listening
+app.listen(port, () => console.log("Server started listening on port " + port + "."));

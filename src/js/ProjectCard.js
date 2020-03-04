1dd72077f503fc6d95b745ec1884994a8be9653d
laurenbarry00 <laurenbarry00@gmail.com>
2020-03-04 15:22:25 -0800

    Added conditional rendering for project image and demo links

--- src/js/ProjectCard.js
+++ src/js/ProjectCard.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class ProjectCard extends Component {
 
   render() {
-    let project, techList, techComp, projectStatus;
+    let project, techList, techComp, projectStatus, imageComp, demoComp;
 
     // check for null, means something went wrong with retrieving the data
     // (most likely the record on Airtable is incomplete)
@@ -14,6 +14,20 @@ class ProjectCard extends Component {
       techComp = techList.map((tech) =>
         <li id={tech} key={tech}>{tech}</li>
       );
+
+      // if there's an image, set the image component
+      if (project.image) {
+        imageComp = <img src={project.image} alt={"Image for " + project.name} />
+      } else {
+        imageComp = null;
+      }
+
+      // if there's a demo available, set the demo link
+      if (project.demo) {
+        demoComp = <a target="_blank" rel="noopener noreferrer" href={project.demo}>Demo</a>
+      } else {
+        demoComp = null;
+      }
     } catch(error) { // render a simple error instead
       return(
         <div className="project-card">
@@ -28,13 +42,14 @@ class ProjectCard extends Component {
     return (
       <div className="project-card">
         <div className="project-title-repo">
-          <a target="_blank" rel="noopener noreferrer" href={project.demo}><h3>{project.name}</h3></a>
+          <h3>{project.name}</h3>
+          {demoComp}
           <a target="_blank" rel="noopener noreferrer" href={project.repo}>GitHub</a>
         </div>
-        <img src={project.image} alt={"Image for " + project.name} />
+        {imageComp}
         <ul className="project-tech-list">{techComp}</ul>
         <p className="project-blurb">{project.blurb}</p>
-        <p className={projectStatus + " project-status"}>{projectStatus}</p>
+        <p className={projectStatus + " project-status"}>{projectStatus.charAt(0).toUpperCase() + projectStatus.slice(1)}</p>
       </div>
     )
   }

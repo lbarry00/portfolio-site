2cb05d62507a01ef1b50be50c7b90324a147c70e
Leo Barry <github.q56mq@simplelogin.com>
2026-07-02 20:40:47 -0700

    Add 404 page content

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+
 function NotFound() {
-  return <>404 Not Found</>;
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row">
+        <img
+          src="./img/suki.jpg"
+          className="max-w-md rounded-lg shadow-2xl"
+        />
+        <div>
+          <h1 className="text-8xl font-bold">404</h1>
+          <h1 className="text-4xl font-bold">Not Found</h1>
+          <p className="py-6">You know the drill, the page you were looking for might have been moved or deleted.</p>
+          <p className="py-6">Have a Suki photo before you go!</p>
+          <Link
+            to="/"
+            className="btn btn-primary">
+            Return Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;

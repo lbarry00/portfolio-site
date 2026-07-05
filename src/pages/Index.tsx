69e18b6e8c52e1421696fe3c07db6b49247ab2b7
Leo Barry <github.q56mq@simplelogin.com>
2026-07-04 18:02:50 -0700

    Add responsive layout for global layout and index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,13 @@
 function Index() {
   return (
-    <div className="flex-1 bg-base-200 flex justify-center items-center gap-10">
+    <div className="flex-1 bg-base-200 flex flex-col lg:flex-row justify-center items-center gap-10 m-10">
       <img
         src="./img/tianmen.jpg"
-        className="max-w-lg rounded-lg shadow-lg shadow-black"
+        className="lg:max-w-lg max-w-2/3 rounded-lg shadow-lg shadow-black"
       />
-      <div>
+      <div className="lg:text-left text-center">
         <h1 className="text-8xl font-bold">Hello! 👋</h1>
-        <p className="py-6 text-lg">I'm Leo, a software engineer from Seattle, WA.</p>
+        <p className="py-6 lg:text-2xl text-4xl">I'm Leo, a software engineer from Seattle, WA.</p>
       </div>
     </div>
   );
--- src/shared/GlobalLayout.tsx
+++ src/shared/GlobalLayout.tsx
@@ -20,16 +20,79 @@ function GlobalLayout() {
 
   return (
     <div className="min-h-screen flex flex-col text-base-content transition-colors duration-400 ease-in-out">
-      <header className="navbar w-screen bg-base-100 text-base-content p-4 font-bold shadow-md">
-        <div className="flex-1">
-          <Link
-            to="/"
-            className=" flex-1 text-2xl ml-4">
-            LEO BARRY
-          </Link>
+      <div className="drawer flex-1 ">
+        <input
+          id="nav-drawer"
+          type="checkbox"
+          className="drawer-toggle"
+        />
+        <div className="drawer-content flex flex-col flex-1 ">
+          {/* Navbar */}
+          <div className="navbar w-screen bg-base-100 text-base-content p-4 font-bold shadow-md lg:h-auto min-h-30">
+            <div className="flex-none lg:hidden">
+              <label
+                htmlFor="nav-drawer"
+                aria-label="open sidebar"
+                className="btn btn-square btn-ghost drawer-button">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block h-6 w-6 stroke-current">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+              </label>
+            </div>
+            <div className="mx-2 flex-1 px-2">
+              <Link
+                to="/"
+                className="flex-1 lg:text-2xl text-4xl ml-4">
+                LEO BARRY
+              </Link>
+            </div>
+            <div className="hidden flex-none lg:block">
+              <ul className="menu menu-horizontal">
+                <li>
+                  <Link to="/">HOME</Link>
+                </li>
+                <li>
+                  <Link to="/about">ABOUT</Link>
+                </li>
+                <li>
+                  <a href="">RESUME</a>
+                </li>
+                <li>
+                  <Link to="/contact">CONTACT</Link>
+                </li>
+                <li>
+                  <label className="swap swap-rotate">
+                    <input
+                      type="checkbox"
+                      onChange={handleToggleTheme}
+                      checked={theme === "dark"}
+                    />
+                    <SunIcon />
+                    <MoonIcon />
+                  </label>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <main className="flex-1 overflow-auto flex bg-base-200">
+            <Outlet />
+          </main>
         </div>
-        <div className="flex-none mr-4">
-          <ul className="menu menu-horizontal gap-4 p-0 text-sm">
+        <div className="drawer-side">
+          <label
+            htmlFor="nav-drawer"
+            aria-label="close sidebar"
+            className="drawer-overlay"></label>
+          <ul className="menu bg-base-100 min-h-full w-80 p-4 gap-6 text-3xl">
+            {/* Sidebar content here */}
             <li>
               <Link to="/">HOME</Link>
             </li>
@@ -42,21 +105,21 @@ function GlobalLayout() {
             <li>
               <Link to="/contact">CONTACT</Link>
             </li>
-            <label className="swap swap-rotate">
-              <input
-                type="checkbox"
-                onChange={handleToggleTheme}
-                checked={theme === "dark"}
-              />
-              <SunIcon />
-              <MoonIcon />
-            </label>
+            <li className="my-6">
+              <label className="flex cursor-pointer gap-2">
+                <SunIcon />
+                <input
+                  type="checkbox"
+                  className="toggle toggle-xl"
+                  onChange={handleToggleTheme}
+                  checked={theme === "dark"}
+                />
+                <MoonIcon />
+              </label>
+            </li>
           </ul>
         </div>
-      </header>
-      <main className="flex-1 overflow-auto flex">
-        <Outlet />
-      </main>
+      </div>
     </div>
   );
 }
